a9cadfd53d359cf1b6a5cd7b1a7f13d815512a56
Selim <selim-kune-do@hotmail.fr>
2021-03-14 02:14:19 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello-City</title>
+
+    </head>
+    <body class="antialiased">
+        <h1>Hello From Nancy</h1>
+
+        <p>Its currently {{date('h:i')}}</p>
+
+        <footer>
+            &copy;Copy Right {{date('Y')}}  selim ozkan &mid; <a href="#">About Us</a>
+        </footer>
+    </body>
+</html>
